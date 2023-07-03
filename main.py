937d6b4d724ebfdbc26ed9aa66b6472d490ad53d
mahykari <mahyar.karimi2079@gmail.com>
2023-07-03 16:00:45 +0200

    update main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from learner import Learner, NN 
+import torch
+
+from envs import SimpleEnv
+from learner import ReachLearner
 from verifier import ReachVerifier
 
 
@@ -7,13 +10,32 @@ MAX_CEGIS_ITER = 10
 # TODO:
 #  1. a configuration file for `main` (e.g. JSON file).  
 
+
+def sample_env():
+  # 500 2-dimensional samples from the state space.
+  x = torch.rand(500, 2)*2 - 1
+
+  tgt_mask = (
+      (torch.abs(x[:,0]) <= 0.05) & (torch.abs(x[:,1]) <= 0.05)
+  )
+
+  # Partitioning samples to target and decrease sets.
+  x_tgt = x[tgt_mask] 
+  x_dec = x[~tgt_mask] 
+  print(f'|x_dec|={len(x_dec)}, |x_tgt|={len(x_tgt)}')
+  return x_tgt, x_dec
+
+
 def main():
-  learner = Learner(NN(), NN())
+  env = SimpleEnv()
+  learner = ReachLearner(env)
   verifier = ReachVerifier()
 
+
+  x_tgt, x_dec = sample_env()
   # CEGIS loop
   for _ in range(MAX_CEGIS_ITER):
-    learner.fit()
+    learner.fit(x_tgt, x_dec)
     if not verifier.cexs():
       break
 
