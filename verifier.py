45397a49851ae3dd6978acf10680c771dad46b20
Kaushik MALLIK <kmallik@lntohe034.ist.local>
2023-07-10 18:30:10 +0200

    wip

--- verifier.py
+++ verifier.py
@@ -1,7 +1,13 @@
 from typing import Set, TypeAlias
+
+import numpy
 from maraboupy import Marabou
 from maraboupy import MarabouCore
 import numpy as np
+from pytope import Polytope
+from scipy.optimize import linprog
+# import cdd  # pycddlib -- for vertex enumeration from H-representation
+# from scipy.spatial import ConvexHull  # for finding A, b from V-representation
 
 
 # Placeholder type for Counter-Example
@@ -12,15 +18,17 @@ OPT = Marabou.createOptions(timeoutInSeconds=10,verbosity=0)
 
 
 class Verifier:
-  def __init__(self, f, network):
-    self.network = network
+  def __init__(self, f, certificate, grid):
     self.f = f
+    self.certificate = certificate
+    self.grid = grid
 
-  def reset_network(self, network):
-    self.network = network
+  def reset_networks(self, certificate, grid):
+    self.certificate = certificate
+    self.grid = grid
 
   # Find counter-examples for a given learner
-  def cexs(self) -> Set[CEx]:
+  def verify_certificate(self) -> Set[CEx]:
     return set()
 
   # Verify certificate condition(s) on a given partition element
@@ -29,63 +37,71 @@ class Verifier:
 
   # Compute the over-approximation of the one-step reachable set
   # Inputs: f = transition function, xs = set of initial states (polytope)
-  def reach_set(self, f, xs):
-    return xs # placeholder (return the current set of states)
+  def reach_set_linear_system(self, a, xs):
+    # g = xs[0]
+    # h = xs[1]
+    ys = a * xs
+    return ys
+
 
 
 # Verifier for reachability (Reach) properties
 class ReachVerifier(Verifier):
-    def __init__(self, f, network, x_target=None):
-        super().__init__(f, network)
-        self.x_target = x_target if x_target else None
+  def __init__(self, f, certificate, grid, x_target=None):
+    super().__init__(f, certificate, grid)
+    self.x_target = x_target if x_target else None
 
-    def cexs(self) -> Set[CEx]:
-        return set()
+  def verify_certificate(self):
+    # Enumerate over every partition to verify them
+    # l = self.grid.equList
+    # for equ in l
+    #   result = linprog()
+    # return set()
 
-  # A partition element is specified as a tuple (g, h, v),
-  # such that the partition is given by {x | gx <= h },
-  # and the value of the claimed (strict) lower bound on the Lypaunov function's value is v
+  # A partition element is a pair of a Polytope p and
+  # the value of the claimed (strict) lower bound on the Lypaunov function's value is v
   def verify_partition(self, partition):
-    network = self.network
+    certificate = self.certificate
     # Step 1.1: clear the user-defined constraints from the last round
-    network.clearProperty()
-    # Step 1.2: input constraints on the network
-    input_vars = network.inputVars
-    g = partition[0]
-    h = partition[1]
-    v = partition[2]
+    certificate.clearProperty()
+    # Step 1.2: input constraints on the certificate
+    input_vars = certificate.inputVars
+    state_set = partition[0]
+    g = state_set.A
+    h = state_set.B
+    v = partition[1]
     for row in g:
-        network.addInequality(input_vars, g[row], h[row])
-    # Step 1.3: output constraints on the network
-    output_vars = network.outputVars
-    network.setUpperBound(output_vars[0], v)
+      certificate.addInequality(input_vars, g[row], h[row])
+    # Step 1.3: output constraints on the certificate
+    output_vars = certificate.outputVars
+    certificate.setUpperBound(output_vars[0], v)
     # Step 1.4: solve the constraints
-    res1, val1, _ = network.solve(verbose=False, options=OPT)
+    res1, val1, _ = certificate.solve(verbose=False, options=OPT)
     if res1 == 'sat':
-        # counter-example found: value of the Lyapunov function is less than v
-        cex1 = {val1}
+      # counter-example found: value of the Lyapunov function is less than v
+      cex1 = {val1}
     else:
-        cex1 = {}
+      cex1 = {}
     # Step 2.1: clear the user-defined constraints from the last round
-    network.clearProperty()
+    certificate.clearProperty()
     # Step 2.2: compute the over-approximation of the set of post states
-    post = self.reach_set(self.f, partition) # f is the dynamics, partition is the set of current states, post is a polytope
+    post_state_set = self.reach_set_linear_system(self.f, state_set) # f is the dynamics, partition is the set of current states, post is a polytope
     # Step 2.3: input constraints
-    H = post[0]
-    h = post[1]
-    for row in H:
-        network.addInequality(input_vars, H[row], h[row])
-    # Step 2.4: output constraints on the network
-    network.setLowerBound(output_vars[0], v)
+    g = post_state_set.A
+    h = post_state_set.b
+    for row in g:
+      certificate.addInequality(input_vars, g[row], h[row])
+    # Step 2.4: output constraints on the certificate
+    certificate.setLowerBound(output_vars[0], v)
     # Step 2.5: solve the constraints
-    res2, val2, _ = network.solve(verbose=False, options=OPT)
+    res2, val2, _ = certificate.solve(verbose=False, options=OPT)
     if res2 == 'sat':
-        # counter-example found: value of the Lyapunov function of the post states is greater than v
-        cex2 = { val2 }
+      # counter-example found: value of the Lyapunov function of the post states is greater than v
+      cex2 = { val2 }
     else:
-        cex2 = {}
+      cex2 = {}
     # Step 3: combine the results
     if res1 == 'unsat' and res2 == 'unsat':
-        return ['unsat']
+      return ['unsat']
     else:
-        return ['sat', cex1, cex2]
+      return ['sat', cex1, cex2]
