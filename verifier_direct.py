63953865f3bbcd52fbc9ba59ee2dbd7dd4d49406
mahykari <mahyar.karimi2079@gmail.com>
2023-07-12 15:12:21 +0200

    update intersect

--- verifier_direct.py
+++ verifier_direct.py
@@ -2,32 +2,45 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from pytope import Polytope
 from scipy.optimize import linprog
-# import cdd  # pycddlib -- for vertex enumeration from H-representation
-# from scipy.spatial import ConvexHull  # for finding A, b from V-representation
 
 
-# check if the region created by intersection of two sets of hyperspaces is empty, using linear programming
-# hs1 and hs2 are given as tuples (H, h), representing the regions as { x | Hx <= h}
-def intersect(hs1, hs2):
-  if hs1.shape[1] != hs2.shape[1]:
-    raise ValueError("invalid argument: the hyperspaces provided are in different dimensions")
-  else:
-    dim = hs1.shape[1]
+def intersect(hs1, hs2) -> bool:
+  """Check if the intersection of two sets of hyperspaces is 
+  non-empty.
+  Args: 
+    hs1, hs2: first (and second) set of halfspaces. Each of hs1 and 
+    hs2 is given as a tuple (H, h), where H is a 2D-matrix and h is a 
+    vector, such that the specified region is { x | Hx <= h }. 
+  """
+  INF = 1e3  # Assumption. -INF <= x <= INF. 
 
-  c = np.zeros(dim)
-  a = np.concatenate((hs1[0], hs2[0]), axis=0)
-  b = np.concatenate((hs1[1], hs2[1]), axis=0)
+  H1, h1 = hs1 
+  H2, h2 = hs2
 
-  res = linprog(c, a, b)
+  assert H1.shape[1] == H2.shape[1], \
+    "provided hyperspaces are in different dimensions"
+  dim = H1.shape[1]
 
-  if res.status == 2: # infeasible
-    return 'infeasible'
-  elif res.status == 0:
-    return a, b
-  else:
-    raise ValueError("inconclusive result of the linear programming problem")
+  # Checking if the intersection is non-empty is equivalent with 
+  # minimizing x such that H1x <= h1 and H2x <= h2. To find such x, 
+  # we use scipy.optimize.linprog. For further information on 
+  # linprog, visit the official documentation of scipy.optimize at 
+  # the following link:
+  # https://docs.scipy.org/doc/scipy/reference/optimize.html
+  c = np.ones(dim)
+  A_ub = np.concatenate((H1, H2), axis=0)
+  b_ub = np.concatenate((h1, h2), axis=0)
+  bounds = [(-INF, INF) for _ in range(dim)]
+  res = linprog(c, A_ub, b_ub, bounds=bounds)
+
+  return res.success
+  # if res.status == 2: # infeasible
+  #   return 'infeasible'
+  # elif res.status == 0:
+  #   return a, b
+  # else:
+  #   raise ValueError("inconclusive result of the linear programming problem")
 
 class Verifier:
   def __init__(self, f, certificate, grid):
@@ -40,7 +53,7 @@ class Verifier:
     self.grid = grid
 
   # Find counter-examples for a given learner
-  def verify_certificate(self) -> Set[CEx]:
+  def verify_certificate(self) -> ...:
     return set()
 
   # Verify certificate condition(s) on a given partition element
@@ -63,7 +76,7 @@ class ReachVerifier(Verifier):
   def verify_certificate(self):
     # Step 1: Collect all partitions created by the Lyapunov function, using build_config_tree()
     # Step 2: For every partition, check if the decrease condition of the Lyapunov function is satisfied
-
+    ...
 
   # Build the configuration tree.
   # Every node consists of the following tuple: (x, c),
@@ -72,7 +85,7 @@ class ReachVerifier(Verifier):
   # the state space xs is given as a polytope in h-representation (H,h)
   def build_config_tree(self, xs):
     t = Tree()
-    c = # initialize a list of lists, where every list in c is the list of configurations of the nodes in a distinct layer
+    c = ...  # initialize a list of lists, where every list in c is the list of configurations of the nodes in a distinct layer
     # iterate over every node and populate the tree
     # named_layers = dict(certificate.named_modules())
     # for layer in named_layers.values(): # iterate over each layer
@@ -91,19 +104,19 @@ class ReachVerifier(Verifier):
         nc[i] = [1, 0] # pair of weight and bias in the input layer
       else:
         c = np.diag(config[i])
-        nc[i][0] = # c * (weight vector of neurons in layer i) * (nc[0] + nc[1])
-        nc[i][1] = # c * (bias vector of neurons in layer i)
+        nc[i][0] = ...  # c * (weight vector of neurons in layer i) * (nc[0] + nc[1])
+        nc[i][1] = ...  # c * (bias vector of neurons in layer i)
 
     return nc
 
   # determine the invariant configuration due to the node node_id whose status is given in activation
   def hyperspace_input_domain(self, node_id, config, activation):
-    nc = node_weight_and_bias(node_id, config)
+    nc = self.node_weight_and_bias(node_id, config)
     d = np.diag(2*config[node_id[0]]-1)
     if activation == 'TRUE':
-      hs = # [ - d * weights[node_id[0]] * nc[node_id[0]-1][0], d * bias[node_id] - weights[node_id[0]]*nc[node_id[0]-1][1] ]
+      hs = ...  # [ - d * weights[node_id[0]] * nc[node_id[0]-1][0], d * bias[node_id] - weights[node_id[0]]*nc[node_id[0]-1][1] ]
     elif activation == 'FALSE':
-      hs =  # [ d * weights[node_id[0]] * nc[node_id[0]-1][0], - d * bias[node_id] - weights[node_id[0]]*nc[node_id[0]-1][1] ]
+      hs = ...  # [ d * weights[node_id[0]] * nc[node_id[0]-1][0], - d * bias[node_id] - weights[node_id[0]]*nc[node_id[0]-1][1] ]
 
     return hs
 
