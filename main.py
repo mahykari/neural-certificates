48f9e78591f39c97c0e95d26ad390f677c632991
mahykari <mahyar.karimi2079@gmail.com>
2023-08-21 19:17:04 +0200

    update main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from envs import Spiral
+from envs import Spiral, F_Spiral
 from learner import Learner_Reach_C
 from verifier import Verifier_Reach_C
 
@@ -26,13 +26,12 @@ logger.setLevel(logging.DEBUG)
 def main():
   env = Spiral()
   C_tgt, C_dec = env.sample()
-  verifier = Verifier_Reach_C()
 
   # CEGIS loop
   # TODO. Check if the internal training loop can be removed.
   for _ in range(MAX_CEGIS_ITER):
     logger.info('CEGIS. Next iter.')
-    learner = None 
+    learner = Learner_Reach_C(env, reach_nn()) 
     for i in range(MAX_TRAIN_ITER):
       new_lrnr = Learner_Reach_C(env, reach_nn())
       new_lrnr.fit(C_tgt, C_dec)
@@ -42,7 +41,10 @@ def main():
         break
     if not learner:
       raise RuntimeError('no learner passed the training check')
-    if not verifier.cexs():
+    verifier = Verifier_Reach_C(learner.cert, env, F_Spiral)
+    cex_dec = verifier.chk_dec()
+    logger.info(f'CEGIS. cex_dec={cex_dec}')
+    if not cex_dec:
       break
 
 
