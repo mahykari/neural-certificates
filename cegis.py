c1071dcdfbc951157e34fd4623c156ed0a9d8dbd
mahykari <mahyar.karimi2079@gmail.com>
2023-08-29 14:19:22 +0200

    update cegis to use Learner_Reach_ABV

--- cegis.py
+++ cegis.py
@@ -2,11 +2,16 @@ import logging
 
 import torch
 
-from envs import Spiral, F_Spiral
-from learner import Learner_Reach_C
+from envs import (
+  Spiral, F_Spiral,
+  SuspendedPendulum, F_SuspendedPendulum)
+from learner import Learner_Reach_C, Learner_Reach_ABV
 from verifier import Verifier_Reach_C
 
-from learner import nn_cert_2d
+from learner import (
+  nn_abst_2d,
+  nn_bound_2d,
+  nn_cert_2d)
 
 
 MAX_CEGIS_ITER = 10
@@ -26,43 +31,45 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def find_learner(env, C_tgt, C_dec): 
+def find_learner(env, S): 
   # TODO. Check if the internal training loop can be removed.
   for i in range(MAX_TRAIN_ITER):
     logger.debug('Next learning iter.')
-    learner = Learner_Reach_C(env, nn_cert_2d())
-    learner.fit(C_tgt, C_dec)
-    if learner.chk(C_tgt, C_dec):
+    learner = Learner_Reach_ABV(
+      env,
+      nn_abst_2d(),
+      nn_bound_2d(),
+      nn_cert_2d())
+    learner.fit(S)
+    if learner.chk(S):
       logger.debug(f'Learner found at iter. {i}.')
       return learner
   return None
 
 
 def main():
-  env = Spiral()
-  # C_tgt, C_dec = env.sample()
-  # As C_tgt is no longer actively used, it is replaced with a 
-  # random Tensor. This is a *temporary* solution.
-  C_tgt, C_dec = torch.rand(1000, 2), env.sample()
-
+  env = SuspendedPendulum()
+  
+  S = env.sample()
   # TODO. Tgt condition is not needed. Make changes accordingly.
   # CEGIS loop
   for _ in range(MAX_CEGIS_ITER):
-    logger.info(f'Next CEGIS iter. |C_dec|={len(C_dec)}')
-    learner = find_learner(env, C_tgt, C_dec)
+    logger.info(f'Next CEGIS iter. |S|={len(S)}')
+    learner = find_learner(env, S)
     if not learner:
       raise RuntimeError(
         f'no learner found after {MAX_TRAIN_ITER} iterations')
-    verifier = Verifier_Reach_C(learner.cert, env, F_Spiral)
-    cex_dec = verifier.chk_dec()
+    # TODO. Implement Verifier_Reach_ABV.
+    # verifier = Verifier_Reach_C(learner.cert, env, F_Spiral)
+    # cex_dec = verifier.chk_dec()
 
-    if not cex_dec:
-      logger.info('No new CEx.')
-      break
-    logger.debug(
-      'CEGIS. Decrease CEx=[' +
-      ', '.join(map(lambda x: f'{x:10.6f}', cex_dec)) + ']' ) 
-    C_dec = torch.vstack([C_dec, torch.Tensor(cex_dec)])
+    # if not cex_dec:
+    #   logger.info('No new CEx.')
+    #   break
+    # logger.debug(
+    #   'CEGIS. Decrease CEx=[' +
+    #   ', '.join(map(lambda x: f'{x:10.6f}', cex_dec)) + ']' ) 
+    # C_dec = torch.vstack([C_dec, torch.Tensor(cex_dec)])
 
 
 if __name__ == '__main__':
