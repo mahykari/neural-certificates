0517430f33dc6dce16fbab305e8680969b3edb98
mahykari <mahyar.karimi2079@gmail.com>
2023-09-05 14:38:18 +0200

    update cegis

--- cegis.py
+++ cegis.py
@@ -31,7 +31,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def find_learner(env, S): 
+def find_learner(env, S):
   # TODO. Check if the internal training loop can be removed.
   for i in range(MAX_TRAIN_ITER):
     logger.debug('Next learning iter.')
@@ -48,13 +48,14 @@ def find_learner(env, S):
 
 
 def main():
-  env = SuspendedPendulum()
-  
+  env = Spiral()
+
   S = env.sample()
   # TODO. Tgt condition is not needed. Make changes accordingly.
   # CEGIS loop
-  for _ in range(MAX_CEGIS_ITER):
-    logger.info(f'Next CEGIS iter. |S|={len(S)}')
+  for i in range(MAX_CEGIS_ITER):
+    N = len(S)
+    logger.info(f'CEGIS iter {i}. |S|={len(S)}')
     learner = find_learner(env, S)
     if not learner:
       raise RuntimeError(
@@ -63,31 +64,30 @@ def main():
     verifier = Verifier_Reach_ABV(
       learner.A,
       learner.B,
-      learner.V, 
-      env, 
-      F_SuspendedPendulum)
+      learner.V,
+      env,
+      F_Spiral)
+    logger.info('CEGIS. Checking Decrease condition ...')
+    cex_dec = verifier.chk_dec()
+    if not cex_dec:
+      logger.info('No new CEx for Decrease.')
+    else:
+      logger.debug(
+        'CEGIS. Decrease CEx=[' +
+        ', '.join(map(lambda x: f'{x:10.6f}', cex_dec)) + ']')
+      cex_dec = torch.randn(N // 10, env.dim) + torch.Tensor(cex_dec)
+    S = torch.vstack([S, cex_dec])
+
+    logger.info('CEGIS. Checking Abstraction condition ...')
     cex_abst = verifier.chk_abst()
     if not cex_abst:
       logger.info('No new CEx for Abstraction.')
-      break
-    logger.debug(
-      'CEGIS. Abstraction CEx=[' +
-      ', '.join(map(lambda x: f'{x:10.6f}', cex_abst)) + ']' )
-    # TODO. Call the rest of cex_... methods.
-    # TODO. Add new samples to S.
-    
-    #################################################################
-    # STALE CODE! 
-    #################################################################
-    # cex_dec = verifier.chk_dec()
-
-    # if not cex_dec:
-    #   logger.info('No new CEx.')
-    #   break
-    # logger.debug(
-    #   'CEGIS. Decrease CEx=[' +
-    #   ', '.join(map(lambda x: f'{x:10.6f}', cex_dec)) + ']' ) 
-    # C_dec = torch.vstack([C_dec, torch.Tensor(cex_dec)])
+    else:
+      logger.debug(
+        'CEGIS. Abstraction CEx=[' +
+        ', '.join(map(lambda x: f'{x:10.6f}', cex_abst)) + ']')
+      cex_abst = torch.randn(N // 10, env.dim) + torch.Tensor(cex_abst)
+    S = torch.vstack([S, cex_abst])
 
 
 if __name__ == '__main__':
