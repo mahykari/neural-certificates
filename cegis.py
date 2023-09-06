a76761c22e5d2687035083067a8b1c2ce5a21eea
mahykari <mahyar.karimi2079@gmail.com>
2023-09-06 17:03:57 +0200

    update cegis; env, learner, and verifier can be set with argv

--- cegis.py
+++ cegis.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import torch
 
@@ -6,7 +7,11 @@ from envs import (
   Spiral, F_Spiral,
   SuspendedPendulum, F_SuspendedPendulum)
 from learner import Learner_Reach_C, Learner_Reach_ABV
-from verifier import Verifier_Reach_C, Verifier_Reach_ABV
+from verifier import (
+  Verifier_Reach_C,
+  Verifier_Reach_ABV_Marabou,
+  Verifier_Reach_ABV_Z3,
+)
 
 from learner import (
   nn_abst_2d,
@@ -17,6 +22,35 @@ from learner import (
 MAX_CEGIS_ITER = 10
 MAX_TRAIN_ITER = 10
 
+ENVS = {
+  'Spiral': Spiral,
+  'SuspendedPendulum': SuspendedPendulum,
+}
+
+FS = {
+  'Spiral': F_Spiral,
+  'SuspendedPendulum': F_SuspendedPendulum,
+}
+
+LEARNERS = {
+  'C': Learner_Reach_C,
+  'ABV': Learner_Reach_ABV,
+}
+
+MODELS = {
+  'C': [nn_cert_2d],
+  'ABV': [nn_abst_2d, nn_bound_2d, nn_cert_2d]
+}
+
+VERIFIERS = {
+  'C': {
+    'Z3': Verifier_Reach_C
+  },
+  'ABV': {
+    'Marabou': Verifier_Reach_ABV_Marabou,
+    'Z3': Verifier_Reach_ABV_Z3,
+  },
+}
 
 # Logging setup
 root_logger = logging.getLogger('')
@@ -31,63 +65,30 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def find_learner(env, S):
-  # TODO. Check if the internal training loop can be removed.
-  for i in range(MAX_TRAIN_ITER):
-    logger.debug('Next learning iter.')
-    learner = Learner_Reach_ABV(
-      env,
-      nn_abst_2d(),
-      nn_bound_2d(),
-      nn_cert_2d())
-    learner.fit(S)
-    if learner.chk(S):
-      logger.debug(f'Learner found at iter. {i}.')
-      return learner
-  return None
-
-
 def main():
-  env = Spiral()
+  envname, modelcomb, solver = sys.argv[1:4]
+  env, F_env = ENVS[envname](), FS[envname]
 
   S = env.sample()
   # TODO. Tgt condition is not needed. Make changes accordingly.
   # CEGIS loop
   for i in range(MAX_CEGIS_ITER):
     N = len(S)
-    logger.info(f'CEGIS iter {i}. |S|={len(S)}')
-    learner = find_learner(env, S)
-    if not learner:
-      raise RuntimeError(
-        f'no learner found after {MAX_TRAIN_ITER} iterations')
-
-    verifier = Verifier_Reach_ABV(
-      learner.A,
-      learner.B,
-      learner.V,
-      env,
-      F_Spiral)
-    logger.info('CEGIS. Checking Decrease condition ...')
-    cex_dec = verifier.chk_dec()
-    if not cex_dec:
-      logger.info('No new CEx for Decrease.')
+    logger.info(f'Iteration {i}. |S|={len(S)}')
+    models = [m() for m in MODELS[modelcomb]]
+    learner = LEARNERS[modelcomb](env, models)
+    learner.fit(S)
+    verifier = VERIFIERS[modelcomb][solver](models, env, F_env)
+    # Cexs is a list of points.
+    cexs = verifier.chk()
+    if not cexs:
+      logger.info('No new CExs found.')
     else:
-      logger.debug(
-        'CEGIS. Decrease CEx=[' +
-        ', '.join(map(lambda x: f'{x:10.6f}', cex_dec)) + ']')
-      cex_dec = torch.randn(N // 10, env.dim) + torch.Tensor(cex_dec)
-      S = torch.vstack([S, cex_dec])
-
-    logger.info('CEGIS. Checking Abstraction condition ...')
-    cex_abst = verifier.chk_abst()
-    if not cex_abst:
-      logger.info('No new CEx for Abstraction.')
-    else:
-      logger.debug(
-        'CEGIS. Abstraction CEx=[' +
-        ', '.join(map(lambda x: f'{x:10.6f}', cex_abst)) + ']')
-      cex_abst = torch.randn(N // 10, env.dim) + torch.Tensor(cex_abst)
-      S = torch.vstack([S, cex_abst])
+      logger.debug(f'CExs={cexs}')
+      cexs = [
+        torch.randn(N // 10, env.dim) + torch.Tensor(cex)
+        for cex in cexs]
+      S = torch.vstack([S] + cexs)
 
 
 if __name__ == '__main__':
