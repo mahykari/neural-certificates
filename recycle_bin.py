6f05ea998ee3f203d08b7014bb334705e161af13
mahykari <mahyar.karimi2079@gmail.com>
2023-09-20 14:59:16 +0200

    update recycle_bin

--- recycle_bin.py
+++ recycle_bin.py
@@ -1,7 +1,9 @@
 from typing import List, Callable
 
 import torch 
-import torch.nn as nn 
+import torch.nn as nn
+
+from verifier import Wb
 
 
 def binary_search(a, n, f):
@@ -64,3 +66,78 @@ def label_strong_dec(
     else:
       P[idx].append(X[i])
   return P
+
+
+def nn_norm_l1(dim: int):
+  """NN to compute L1 norm of a vector [x1, ..., xk], where k = dim.
+
+  Args:
+    dim: dimensionality of input vector.
+  """
+  net = nn.Sequential(
+    nn.Linear(dim, 2 * dim, bias=False),
+    nn.ReLU(),
+    nn.Linear(4, 1, bias=False),
+  )
+
+  with torch.no_grad():
+    net[0].weight = nn.Parameter(
+      torch.hstack([torch.eye(2, 2), torch.eye(2, 2) * -1]).T)
+    net[2].weight = nn.Parameter(torch.ones(1, 4))
+
+  return net
+
+
+class ABVComposite(nn.Module):
+  """Composite network containing A, B, and V. This network takes
+  (x, y) as input, where x is a sample from the state space and y is
+  an error variable, and returns the following as output:
+  ( ||y||_1 - B(x), V(x) - V(A(x) + y) )  (Eq. 1)
+
+  Assumption. Both x and y are passed as 2D-Tensors with only one
+  row and matching number of columns. If this assumption is true,
+  output is also a 2D-Tensor with only one row and two columns.
+  """
+
+  def __init__(self, A, B, V, dim):
+    super().__init__()
+    self.A = A
+    self.B = B
+    self.V = V
+    self.NormL1 = nn_norm_l1(dim)
+
+  def forward(self, x, y):
+    v = self.V(x)
+    vp = self.V(self.A(x) + y)
+    norm_y = self.NormL1(y)
+    b = self.B(x)
+    return torch.cat([norm_y + -1 * b, v + -1 * vp], dim=1)
+
+
+def v_compose(models):
+  """Vertical composition of NNs with identical layer structure.
+
+  Args:
+    models: a list of nn.Sequential-s
+  """
+  assert len(models) > 0
+  result = nn.Sequential()
+  n_layers = len(models[0])
+  for i in range(n_layers):
+    lyr = models[0][i]
+    match lyr:
+      case nn.ReLU():
+        result.append(nn.ReLU())
+      case nn.Linear():
+        in_f, out_f = lyr.in_features, lyr.out_features
+        Wbs = [Wb(m[i], numpy=False) for m in models]
+        Ws, bs = list(zip(*Wbs))
+        W, b = torch.vstack(Ws), torch.cat(bs)
+        linear = nn.Linear(in_f, out_f)
+        with torch.no_grad():
+          linear.weight = nn.Parameter(W)
+          linear.bias = nn.Parameter(b)
+        result.append(linear)
+      case _:
+        raise NotImplementedError()
+  return result
