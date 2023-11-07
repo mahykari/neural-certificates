4ae5b5ac3b5e726e0a85be3052260739f6ff17d0
Kaushik MALLIK <kmallik@lntohe035.ist.local>
2023-11-07 21:28:57 +0100

    added the learner for progress measure of 3-color parity

--- learner.py
+++ learner.py
@@ -1,13 +1,13 @@
-import logging 
+import logging
 from abc import ABC, abstractmethod
 from typing import List
 
 import torch
 import torch.nn as nn
-import torch.utils.data as D 
+import torch.utils.data as D
 import torch.optim as optim
 
-from envs import Env 
+from envs import Env
 
 
 logger = logging.getLogger(__name__)
@@ -15,8 +15,8 @@ logger.setLevel(logging.DEBUG)
 
 
 def nn_V_2d():
-  """Utility function to generate a default Reach certificate for a 
-  2D space. Zeroing bias terms and using ReLU activation on the last 
+  """Utility function to generate a default Reach certificate for a
+  2D space. Zeroing bias terms and using ReLU activation on the last
   layer enforces Nonnegativity and Target conditions.
   """
   net = nn.Sequential(
@@ -30,7 +30,7 @@ def nn_V_2d():
 
 
 def nn_A_2d():
-  """Utility function to generate a default abstraction NN for a 
+  """Utility function to generate a default abstraction NN for a
   2D space."""
   return nn.Sequential(
     nn.Linear(2, 4),
@@ -40,13 +40,13 @@ def nn_A_2d():
 
 
 def nn_B_2d():
-  """Utility function to generate a default error bound NN for a 
+  """Utility function to generate a default error bound NN for a
   2D space.
-  
-  Note: this network is used to learn the abstraction error 
-  B(x) = || f(x) - A(x) || + eps(x), where f is a transition 
-  function, A is a neural abstraction of f, and eps is a non-negative 
-  error term. Intuitively, B is a tight overapproximation of 
+
+  Note: this network is used to learn the abstraction error
+  B(x) = || f(x) - A(x) || + eps(x), where f is a transition
+  function, A is a neural abstraction of f, and eps is a non-negative
+  error term. Intuitively, B is a tight overapproximation of
   || f - A ||.
   """
   return nn.Sequential(
@@ -57,13 +57,13 @@ def nn_B_2d():
   )
 
 
-# Learners---and similarly, verifiers---are named based on the 
+# Learners---and similarly, verifiers---are named based on the
 # following rule: Learner_<Property>_<Components>.
-# Property can be Reach, Safe, or ReachAvoid. Components is a 
-# (sorted) sequence of characters, where each character shows a 
-# particular NN (or generally, model) in the learner. For instance, 
-# Learner_Reach_AC is a learner for property Reach, and contains a 
-# NN for Abstraction (A) and Certificate (C). 
+# Property can be Reach, Safe, or ReachAvoid. Components is a
+# (sorted) sequence of characters, where each character shows a
+# particular NN (or generally, model) in the learner. For instance,
+# Learner_Reach_AC is a learner for property Reach, and contains a
+# NN for Abstraction (A) and Certificate (C).
 
 class Learner(ABC):
   @abstractmethod
@@ -136,9 +136,9 @@ class Learner_Reach_V(Learner):
     return S
 
   def loss_fn(self, S):
-    """Aggregate loss function for the certificate NN. 
+    """Aggregate loss function for the certificate NN.
 
-    New components can be added to the loss by defining new 
+    New components can be added to the loss by defining new
     functions and calling them in the expression evaluated below.
     """
     return 1e5 * self.loss_dec(S)
@@ -164,26 +164,26 @@ class Learner_Reach_V(Learner):
 
 def sample_ball(dim: int, n_samples: int = 100):
   """Sampled points from the surface of a unit ball.
-  
-  Args: 
+
+  Args:
     dim: dimensions of the ball.
     n_samples: number of samples.
   """
-  # P_S is a Tensor with shape (n, dim), where each element is 
+  # P_S is a Tensor with shape (n, dim), where each element is
   # sampled from the surface of a unit ball.
   points = torch.rand(n_samples, dim)*2 - 1
   # L2-norm of each row in b.
-  # The next steps fix the dimensions of norms, so that norm_b will 
-  # be of shape (n, dim), where all elements in row i are equal to 
-  # the norm of row i in b. 
+  # The next steps fix the dimensions of norms, so that norm_b will
+  # be of shape (n, dim), where all elements in row i are equal to
+  # the norm of row i in b.
   norms = torch.norm(points, dim=1)
   norms = norms.unsqueeze(dim=1)
   norms = norms.expand(n_samples, dim)
-  # Points should be re-scaled to have norm 1. Afterwards, we 
-  # re-scale again to a random values. 
+  # Points should be re-scaled to have norm 1. Afterwards, we
+  # re-scale again to a random values.
   points /= norms
   points *= torch.rand(n_samples, 1)
-  return points 
+  return points
 
 
 class Learner_Reach_ABV(Learner):
@@ -193,7 +193,7 @@ class Learner_Reach_ABV(Learner):
     self.A, self.B, self.V = models
     self.learning_rate = 1e-3
     self.weight_decay = 1e-5
-  
+
   def fit(self, S):
     # N_EPOCH, N_BATCH = 2048, 50
     N_EPOCH, N_BATCH = 64, 50  # For debugging purposes
@@ -214,7 +214,7 @@ class Learner_Reach_ABV(Learner):
       lr=self.learning_rate,
       weight_decay=self.weight_decay,
     )
-    
+
     for e in range(N_EPOCH+1):
       epoch_loss = 0
       ball = sample_ball(2, 1000)
@@ -252,15 +252,15 @@ class Learner_Reach_ABV(Learner):
     err = torch.unsqueeze(err, dim=1)
     return torch.mean(torch.relu(err - self.B(S)))
 
-  # Loss_bound enforces B(s) to be close to zero for all s. 
+  # Loss_bound enforces B(s) to be close to zero for all s.
   # TODO. Check if this function is needed at all.
   def loss_bound(self, S):
     return torch.mean(self.B(S))
 
   def loss_cert(self, ball, S):
     def loss_cert_state(s):
-      # Loss_cert for a single state s. For each s, we construct a 
-      # sample ball with center A(s) and radius B(s); we do so by 
+      # Loss_cert for a single state s. For each s, we construct a
+      # sample ball with center A(s) and radius B(s); we do so by
       # rescaling and shifting samples in ball.
       return torch.sum(torch.relu(
         self.V(self.A(s) + self.B(s)*ball) - self.V(s) + 1))
@@ -273,7 +273,156 @@ class Learner_Reach_ABV(Learner):
       + 1000*self.loss_bound(S)
       + self.loss_cert(ball, S)
     )
-  
+
   def chk(self, S):
     # TODO. Update this check with actual values.
     return True
+
+class Learner_3Parity_P(Learner):
+  def __init__(self, env, models):
+    # Assumption. Cert is a fully-connected NN with ReLU activation
+    # after each hidden layer, as well as the output layer. We can
+    # simply assume cert to be an instance of nn.Sequential,
+    # initialized as follows:
+    # nn.Sequential(
+    #   nn.Linear(...),
+    #   nn.ReLU(),
+    #   ... )
+    self.env = env
+    self.P = models[0]
+
+  def fit(self, S):
+    """Fits P based on a predefined loss function.
+
+    Args:
+      S = [ [S0], [S1], [S2] ]: sets of sampled points from the state space with priorities 0, 1, 2, respectively.
+    """
+    n_epoch = 16
+    batch_size = 1000
+    learning_rate = 1e-3
+
+    self.P.to(self.device)
+    for i in range(3):
+        S[i] = S[i].to(self.device)
+
+    optimizer = optim.SGD(
+      self.P.parameters(),
+      lr=learning_rate)
+
+    for i in range(3):
+        dataloader = D.DataLoader(S[i], batch_size=batch_size, shuffle=True)
+        for e in range(n_epoch + 1):
+          for batch in dataloader:
+            optimizer.zero_grad()
+            loss = self.loss_fn(batch)
+            loss.backward()
+            nn.utils.clip_grad_norm_(self.P.parameters(), 1e3)
+            optimizer.step()
+          if e % (n_epoch >> 3) != 0:
+            continue
+          logger.debug(
+            f'Epoch {e:>5}. '
+            + f'Loss={self.loss_fn(S[i]).item():>12.6f}, '
+            + f'Chk={self.chk(S[i]).item():>8.6f}, '
+            + f'|S{i}|={len(S[i]):>8}, '
+            + f'L.R.={learning_rate:.6f}'
+          )
+
+        torch.cuda.empty_cache()
+        self.P.to('cpu')
+        S[i] = S[i].to('cpu')
+
+
+    return S
+
+  def loss_fn(self, S):
+    """Aggregate loss function for the certificate NN.
+
+    New components can be added to the loss by defining new
+    functions and calling them in the expression evaluated below.
+    """
+    return 1e5 * self.loss_dec(S)
+
+  def loss_dec(self, S, eps=1):
+    """Loss component for the lexicographic decrease condition.
+
+    For any point x, this functions increases the loss if
+    x in S[0]: P(f(x))[0] - P(x)[0] > 0
+    x in S[1]: P(f(x))[0] - P(x)[0] > 0 ||
+               P(f(x))[0] - P(x)[0] + eps > 0 && P(f(x))[1] - P(x)[1] + eps > 0
+    x in S[2]: P(f(x))[0] - P(x)[0] > 0 ||
+               P(f(x))[0] - P(x)[0] + eps > 0 && P(f(x))[1] - P(x)[1] > 0 ||
+               P(f(x))[0] - P(x)[0] + eps > 0 && P(f(x))[1] - P(x)[1] + eps > 0 && P(f(x))[2] - P(x)[2] > 0
+
+    This enforces the lexicographic decrease conditions:
+    x in S[0]: P(x) =>_0 P(f(x))
+    x in S[1]: P(x) >_1  P(f(x))
+    x in S[2]: P(x) =>_2 P(f(x))
+
+    Args:
+      S: a batch of points sampled from outside the target space.
+    """
+    for i in range(3):
+        S_nxt[i] = self.env.f(S[i])
+
+    # Xij for every x in S[i]: positive if P(f(x))[j] - P(x)[j] > 0
+    # Yij for every x in S[i]: positive if P(f(x))[j] - P(x)[j] + eps > 0
+    X00 = torch.relu(self.P(S_nxt[0])[0] - self.P(S[0])[0])
+
+    X10 = torch.relu(self.P(S_nxt[1])[0] - self.P(S[1])[0])
+    Y10 = torch.relu(self.P(S_nxt[1])[0] - self.P(S[1])[0] + eps)
+    Y11 = torch.relu(self.P(S_nxt[1])[1] - self.P(S[1])[1] + eps)
+
+    X20 = torch.relu(self.P(S_nxt[2])[0] - self.P(S[2])[0])
+    X21 = torch.relu(self.P(S_nxt[2])[1] - self.P(S[2])[1])
+    X22 = torch.relu(self.P(S_nxt[2])[2] - self.P(S[2])[2])
+    Y20 = torch.relu(self.P(S_nxt[2])[0] - self.P(S[2])[0] + eps)
+    Y21 = torch.relu(self.P(S_nxt[2])[1] - self.P(S[2])[1] + eps)
+
+    L0 = torch.mean(X00)
+    L1 = torch.mean(
+            torch.maximum( X10, torch.minimum( Y10, Y11 ))
+        )
+    L2 = torch.mean(
+            torch.maximum( X10,
+                torch.maximum(
+                    torch.minimum( Y20, X21 ),
+                    torch.minimum( Y20, torch.minimum( Y21, X22 ))
+                )
+            )
+        )
+
+    return (L0 + L1 + L2)/3
+
+  def chk(self, S, eps=0.01):
+    for i in range(3):
+        S_nxt[i] = self.env.f(S[i])
+
+    # Xij for every x in S[i]: positive if P(f(x))[j] - P(x)[j] > 0
+    # Yij for every x in S[i]: positive if P(f(x))[j] - P(x)[j] + eps > 0
+    X00 = torch.relu(self.P(S_nxt[0])[0] - self.P(S[0])[0])
+
+    X10 = torch.relu(self.P(S_nxt[1])[0] - self.P(S[1])[0])
+    Y10 = torch.relu(self.P(S_nxt[1])[0] - self.P(S[1])[0] + eps)
+    Y11 = torch.relu(self.P(S_nxt[1])[1] - self.P(S[1])[1] + eps)
+
+    X20 = torch.relu(self.P(S_nxt[2])[0] - self.P(S[2])[0])
+    X21 = torch.relu(self.P(S_nxt[2])[1] - self.P(S[2])[1])
+    X22 = torch.relu(self.P(S_nxt[2])[2] - self.P(S[2])[2])
+    Y20 = torch.relu(self.P(S_nxt[2])[0] - self.P(S[2])[0] + eps)
+    Y21 = torch.relu(self.P(S_nxt[2])[1] - self.P(S[2])[1] + eps)
+
+    L0 = torch.max(X00)
+    L1 = torch.max(
+            torch.maximum( X10, torch.minimum( Y10, Y11 ))
+        )
+    L2 = torch.max(
+            torch.maximum( X10,
+                torch.maximum(
+                    torch.minimum( Y20, X21 ),
+                    torch.minimum( Y20, torch.minimum( Y21, X22 ))
+                )
+            )
+        )
+
+    return torch.maximum( L0, torch.maximum( L1, L2) )
