55843b7761c2145b71a5ea6624bad482c4f71746
mahykari <mahyar.karimi2079@gmail.com>
2023-12-21 16:56:08 +0100

    add script for map3x3

---
+++ drafts/map3x3.py
@@ -0,0 +1,49 @@
+import logging
+
+import torch
+from matplotlib import pyplot as plt  # noqa
+
+from learner import Learner_3Parity_P, nn_P_2d
+from envs import Map3x3
+
+# Logging setup
+root_logger = logging.getLogger('')
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    fmt='%(asctime)s - %(module)-8s - %(levelname)-6s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+root_logger.addHandler(handler)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+env = Map3x3()
+
+learner = Learner_3Parity_P(env, [nn_P_2d()])
+
+n_samples = 20000
+X = env.sample(n_samples)
+
+# Marking samples with colors
+C = env.mark(X)
+C = torch.unsqueeze(C, dim=1)
+
+plt.scatter(X[:, 0], X[:, 1], s=1, c=C)
+plt.show()
+plt.close()
+
+S = torch.cat((X, C), dim=1)
+
+learner.fit(S, n_epoch=512, batch_size=1000, lr=7e-3)
+X = env.sample(10 * n_samples)
+C = env.mark(X).unsqueeze(dim=1)
+S = torch.cat((X, C), dim=1)
+
+cl = learner.color_loss(S, eps=0.012345).detach()
+
+print(f'Loss. Max={torch.max(cl)}, Mean={torch.mean(cl)}')
+
+plt.scatter(X[:, 0], X[:, 1], s=1, c=cl > 0)
+plt.show()
+plt.close()
