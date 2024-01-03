bd71c78f3d6e4db3cf3ae23cdfcfba9be29fe149
mahykari <mahyar.karimi2079@gmail.com>
2024-01-03 17:13:07 +0100

    update limit_cycle

--- drafts/limit_cycle.py
+++ drafts/limit_cycle.py
@@ -1,49 +1,63 @@
 import logging
+import sys
 
 import torch
 from matplotlib import pyplot as plt  # noqa
 
-from learner import Learner_3Parity_P, nn_P_2d
+from learner import Learner_3Parity_P, nn_P
 from envs import LimitCycle
 
 # Logging setup
 root_logger = logging.getLogger('')
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    fmt='%(asctime)s - %(module)-8s - %(levelname)-6s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
 root_logger.addHandler(handler)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+
+# For THIS EXACT CONFIGURATION,
+# the values below can be good options:
+# n_epoch = 512, batch_size = 1000, lr = 2 (or its neighborhood)
+
+
+n_epoch, batch_size, lr = sys.argv[1:4]
+n_epoch, batch_size, lr = int(n_epoch), int(batch_size), float(lr)
 
 env = LimitCycle()
 
-learner = Learner_3Parity_P(env, [nn_P_2d()])
+learner = Learner_3Parity_P(env, [nn_P(2)])
 
-n_samples = 20000
-X = env.sample(n_samples)
+n_div = 200
+X = torch.linspace(0, 4 - 1 / n_div, n_div) - 2
+X = torch.cartesian_prod(X, X)
+
+tor, tand = torch.logical_or, torch.logical_and
+mask = tor(
+    tor(X[:, 0] > 0.5, X[:, 1] > 0.5),
+    tor(X[:, 0] < -0.5, X[:, 1] < -0.5))
+mask = tand(mask, tand(
+    tand(X[:, 0] <= 0.75, X[:, 1] <= 0.75),
+    tand(X[:, 0] >= -0.75, X[:, 1] >= -0.75))
+)
+X = X[mask]
 
 # Marking samples with colors
 C = env.mark(X)
 C = torch.unsqueeze(C, dim=1)
 
 plt.scatter(X[:, 0], X[:, 1], s=1, c=C)
+plt.xlim(-2.1, 2.1)
+plt.ylim(-2.1, 2.1)
 plt.show()
 plt.close()
 
 S = torch.cat((X, C), dim=1)
 
-learner.fit(S, n_epoch=64, lr=1.5e0, batch_size=100)
-X = env.sample(10 * n_samples)
-C = env.mark(X).unsqueeze(dim=1)
-S = torch.cat((X, C), dim=1)
+learner.fit(S, n_epoch=n_epoch, batch_size=batch_size, lr=lr)
+# X = env.sample(10 * n_samples)
+# C = env.mark(X).unsqueeze(dim=1)
+# S = torch.cat((X, C), dim=1)
 
-cl = learner.color_loss(S, eps=0.012345).detach()
+cc = learner.color_chk(S, eps=0.012345).detach()
 
-print(f'Loss. Max={torch.max(cl)}, Mean={torch.mean(cl)}')
-
-plt.scatter(X[:, 0], X[:, 1], s=1, c=cl > 0)
+plt.scatter(X[:, 0], X[:, 1], s=1, c=cc == 0)
 plt.show()
 plt.close()
