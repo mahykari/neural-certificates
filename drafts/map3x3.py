65b1dcf4bb6be89513292a5f9c5425e70736b732
mahykari <mahyar.karimi2079@gmail.com>
2024-01-03 15:15:41 +0100

    update map3x3

--- drafts/map3x3.py
+++ drafts/map3x3.py
@@ -1,4 +1,5 @@
 import logging
+import sys  # noqa
 
 import torch
 from matplotlib import pyplot as plt  # noqa
@@ -18,15 +19,43 @@ root_logger.addHandler(handler)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# For THIS EXACT CONFIGURATION,
+# the values below can be good options:
+# n_epoch = 1024, batch_size = 2000, lr = 0.3
+# The learner hits 100% at some point in training,
+# but might actually end in a suboptimal state.
+# We can ignore this for now.
+
+n_epoch, batch_size, lr = sys.argv[1:4]
+n_epoch, batch_size, lr = int(n_epoch), int(batch_size), float(lr)
+
 env = Map3x3()
 
 learner = Learner_3Parity_P(env, [nn_P_2d()])
 
-# n_samples = 20000
-# X = env.sample(n_samples)
-X = torch.linspace(0, 2.99, 100)
+n_div = 100
+X = torch.linspace(0, 3 - 1 / n_div, n_div)
 X = torch.cartesian_prod(X, X)
+
+# Masking out parts of the state space improves learning.
+# In this example, we can mask out states
+# which have an even color
+# and are part of a cycle.
+mask = torch.logical_and(
+    # Exterior of cell [0, 1] * [0, 1]
+    torch.logical_or(
+        X[:, 0] >= 1, X[:, 1] >= 1
+    ),
+    # Exterior of cell [2, 3] * [1, 2]
+    torch.logical_or(
+        X[:, 0] < 2,
+        torch.logical_or(X[:, 1] < 1, X[:, 1] >= 2)
+    )
+)
+X = X[mask]
+
 n_samples = X.shape[0]
+X = X[torch.randperm(n_samples)]
 
 # Marking samples with colors
 C = env.mark(X)
@@ -39,7 +68,7 @@ plt.close()
 S = torch.cat((X, C), dim=1)
 print(S)
 
-learner.fit(S, n_epoch=1024, batch_size=20000, lr=3)
+learner.fit(S, n_epoch=n_epoch, batch_size=batch_size, lr=lr)
 X = env.sample(10 * n_samples)
 C = env.mark(X).unsqueeze(dim=1)
 S = torch.cat((X, C), dim=1)
