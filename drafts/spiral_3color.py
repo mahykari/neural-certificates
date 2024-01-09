6b7c3a94fe5f37d3962960fb8786ce594c7cad7a
mahykari <mahyar.karimi2079@gmail.com>
2024-01-09 16:27:37 +0100

    add spiral_3color

---
+++ drafts/spiral_3color.py
@@ -0,0 +1,60 @@
+import logging
+
+import torch
+from matplotlib import pyplot as plt  # noqa
+
+from learner import Learner_3Parity_P, nn_P
+from envs import Spiral
+
+# Logging setup
+root_logger = logging.getLogger('')
+handler = logging.StreamHandler()
+root_logger.addHandler(handler)
+
+env = Spiral()
+
+learner = Learner_3Parity_P(env, [nn_P(2)])
+
+X = env.sample()
+
+# Marking samples with colors 0, 1, and 2
+# Interior of [-0.1, 0.1] * [-0.1, 0.1]
+mask_2 = 2 * torch.logical_and(
+    torch.abs(X[:, 0]) < 0.1,
+    torch.abs(X[:, 1]) < 0.1
+)
+# Exterior of [-0.1, 0.1] * [-0.1, 0.1] (mask_2),
+# and Interior of [-0.8, 0.8] * [-0.8] * [0.8]
+mask_1 = 1 * torch.logical_and(
+    torch.logical_not(mask_2),
+    torch.logical_and(
+        torch.abs(X[:, 0]) < 0.8,
+        torch.abs(X[:, 1]) < 0.8))
+# Exterior of [-0.8, 0.8] * [-0.8, 0.8]
+mask_0 = torch.logical_or(
+    torch.abs(X[:, 0]) >= 0.8,
+    torch.abs(X[:, 1]) >= 0.8
+)
+assert torch.all(mask_0 * mask_1 * mask_2 == 0)
+C = mask_1 + mask_2
+C = torch.unsqueeze(C, dim=1)
+S = torch.cat((X, C), dim=1)
+print(S)
+
+plt.scatter(X[:, 0], X[:, 1], s=1, c=C)
+plt.show()
+plt.close()
+
+learner.fit(S, n_epoch=64, lr=3e-1)
+
+for i in torch.randint(0, len(S), (10,)):
+  print(
+      f'C = {C[i].item()}, '
+      + f'P(x) = {learner.P(X[i:i+1, :]).detach()}, '
+      + f'P(f(x)) = {learner.P(env.f(X[i:i+1])).detach()}, '
+  )
+
+cc = learner.color_chk(S).detach()
+plt.scatter(X[:, 0], X[:, 1], s=1, c=cc == 0)
+plt.show()
+plt.close()
