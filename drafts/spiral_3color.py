e8d50f90f0b887e3cce1140b2c14b8226a7fbce7
mahykari <mahyar.karimi2079@gmail.com>
2024-01-10 14:59:38 +0100

    update spiral_3color for better check plot

--- drafts/spiral_3color.py
+++ drafts/spiral_3color.py
@@ -17,27 +17,33 @@ learner = Learner_3Parity_P(env, [nn_P(2)])
 
 X = env.sample()
 
-# Marking samples with colors 0, 1, and 2
-# Interior of [-0.1, 0.1] * [-0.1, 0.1]
-mask_2 = 2 * torch.logical_and(
-    torch.abs(X[:, 0]) < 0.1,
-    torch.abs(X[:, 1]) < 0.1
-)
-# Exterior of [-0.1, 0.1] * [-0.1, 0.1] (mask_2),
-# and Interior of [-0.8, 0.8] * [-0.8] * [0.8]
-mask_1 = 1 * torch.logical_and(
-    torch.logical_not(mask_2),
-    torch.logical_and(
-        torch.abs(X[:, 0]) < 0.8,
-        torch.abs(X[:, 1]) < 0.8))
-# Exterior of [-0.8, 0.8] * [-0.8, 0.8]
-mask_0 = torch.logical_or(
-    torch.abs(X[:, 0]) >= 0.8,
-    torch.abs(X[:, 1]) >= 0.8
-)
-assert torch.all(mask_0 * mask_1 * mask_2 == 0)
-C = mask_1 + mask_2
-C = torch.unsqueeze(C, dim=1)
+
+def mask(X):
+  # Marking samples with colors 0, 1, and 2
+  # Interior of [-0.1, 0.1] * [-0.1, 0.1]
+  mask_1 = 1 * torch.logical_and(
+      torch.abs(X[:, 0]) < 0.2,
+      torch.abs(X[:, 1]) < 0.2
+  )
+  # Exterior of [-0.1, 0.1] * [-0.1, 0.1] (mask_2),
+  # and Interior of [-0.8, 0.8] * [-0.8] * [0.8]
+  mask_2 = 2 * torch.logical_and(
+      torch.logical_not(mask_1),
+      torch.logical_and(
+          torch.abs(X[:, 0]) < 0.8,
+          torch.abs(X[:, 1]) < 0.8))
+  # Exterior of [-0.8, 0.8] * [-0.8, 0.8]
+  mask_0 = torch.logical_or(
+      torch.abs(X[:, 0]) >= 0.8,
+      torch.abs(X[:, 1]) >= 0.8
+  )
+  assert torch.all(mask_0 * mask_1 * mask_2 == 0)
+  C = mask_1 + mask_2
+  C = torch.unsqueeze(C, dim=1)
+  return C
+
+
+C = mask(X)
 S = torch.cat((X, C), dim=1)
 print(S)
 
@@ -53,6 +59,12 @@ for i in torch.randint(0, len(S), (10,)):
       + f'P(x) = {learner.P(X[i:i+1, :]).detach()}, '
       + f'P(f(x)) = {learner.P(env.f(X[i:i+1])).detach()}, '
   )
+
+n_div = 100
+X = torch.linspace(0, 2 - 1 / n_div, n_div) - 1
+X = torch.cartesian_prod(X, X)
+C = mask(X)
+S = torch.cat((X, C), dim=1)
 
 cc = learner.color_chk(S).detach()
 plt.scatter(X[:, 0], X[:, 1], s=1, c=cc == 0)
